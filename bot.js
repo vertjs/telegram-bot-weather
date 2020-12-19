265a562b43e61ec4e6eec4b0929553c83849a5c4
vertjs <elena.viktorovna.bogatyreva@gmail.com>
2020-12-20 01:00:17 +0300

    fixed bugs

--- bot.js
+++ bot.js
@@ -2,17 +2,71 @@ require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
 
-const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
-
-const weatherEndpoint = (id) => {
-  return `http://api.openweathermap.org/data/2.5/weather?id=${id}&units=metric&lang=ru&appid=${process.env.APP_ID}`;
+const bot = new TelegramBot(process.env.BOT_TOKEN, {polling: true});
+const weatherEndpoint = (data) => { // запрос данных с openweathermap
+  if(typeof data === 'string') {
+    return `http://api.openweathermap.org/data/2.5/weather?id=${data}&units=metric&lang=ru&appid=${process.env.APP_ID}`;
+  } else if(typeof data === 'object') {
+    let lat=data.latitude
+    let lon = data.longitude
+    return `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&lang=ru&appid=${process.env.APP_ID}`;
+  } else return;
 };
 
-const weatherIcon = (icon) => {
-  return `http://openweathermap.org/img/w/${icon}.png`;
-};
+console.log('Bot has been started')
 
-const weatherHtmlTemplate = (name, main, weather, wind, sys) =>
+bot.onText(/start/, (msg) => { // запуск бота
+  const opts = {
+    reply_markup: JSON.stringify({
+      keyboard: [
+        [{text: 'Найди меня', request_location: true}],
+        [{ text: 'г.Химки' }, { text: 'п.Быково' }],
+        [{ text: 'г.Москва' }, { text: 'г.Нижний Новгород' }],
+      ],
+      resize_keyboard: true
+    })
+  };
+  bot.sendMessage(msg.chat.id, 'Здесь можно узнать прогноз погоды на сегодня', opts);
+});
+
+bot.on('message', msg => msg.text !== '/start' ? sendData(msg) : '')
+
+function sendData(msg) {
+  switch (msg.text) {
+    case 'г.Химки':
+      getWeather(msg.chat.id, '550280')
+      break;
+    case 'п.Быково':
+      getWeather(msg.chat.id, '570298')
+      break;
+    case 'г.Москва':
+      getWeather(msg.chat.id, '524894')
+      break;
+    case 'г.Нижний Новгород':
+      getWeather(msg.chat.id, '520555')
+      break;
+    default:
+      getWeather(msg.chat.id, msg.location)
+  }
+}
+
+function getWeather(chatId, data) { // обработка данных
+  let endpoint;
+  endpoint = weatherEndpoint(data)
+  console.log(endpoint)
+  axios.get(endpoint).then(
+    resp => {
+      const { name, main, weather, wind, sys } = resp.data;
+      bot.sendMessage(chatId, weatherHtmlTemplate(name, main, weather[0], wind, sys), {
+        parse_mode: 'HTML',
+      });
+    });
+}
+
+function weatherHtmlTemplate(name, main, weather, wind, sys) { // перевод данных в html форамт
+  console.log(name)
+  console.log(weather)
+  return (
   `Погода в <b>${name}</b>: 
   ${weather.description}
   Температура: <b> ${Math.round(main.temp)} °C</b>
@@ -20,92 +74,15 @@ const weatherHtmlTemplate = (name, main, weather, wind, sys) =>
   Влажность: <b> ${main.humidity} %</b>
   Скорость ветра: <b>${wind.speed} м/сек</b>
   Восход солнца: <b> ${setDate(sys.sunrise + 10800)}</b>
-  Закат солнца: <b> ${setDate(sys.sunset + 10800)}</b>
-  `;
-
-// Function that gets the weather by the city name
-const getWeather = (chatId, id) => {
-  const endpoint = weatherEndpoint(id);
-  axios.get(endpoint).then(
-    (resp) => {
-      const { name, main, weather, wind, sys } = resp.data;
-      bot.sendMessage(chatId, weatherHtmlTemplate(name, main, weather[0], wind, sys), {
-        parse_mode: 'HTML',
-      });
-    },
-    (error) => {
-      console.log('error', error);
-      bot.sendMessage(chatId, `Упс...для этого города нет информации`, {
-        parse_mode: 'HTML',
-      });
-    }
+  Закат солнца: <b> ${setDate(sys.sunset + 10800)}</b>`
   );
-};
-
-const options = {
-  reply_markup: JSON.stringify({
-    inline_keyboard: [
-      [{ text: 'г.Химки', callback_data: '550280' }],
-      [{ text: 'п.Быково', callback_data: '570298' }],
-      [{ text: 'г.Москва', callback_data: '524894' }],
-      [{ text: 'г.Нижний Новгород', callback_data: '520555' }],
-    ],
-  }),
-};
-
-/* bot.onText(/\//, (msg, match) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, `Выберите город, в котором хотите узнать прогноз погоды: `, options, {
-    parse_mode: 'HTML',
-  });
-}); */
-bot.onText(/\//, (msg, match) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, `Выберите город, в котором хотите узнать прогноз погоды: `, {
-    reply_markup: {
-      keyboard: [
-        [{ text: 'г.Химки' }, { text: 'п.Быково' }],
-        [{ text: 'г.Москва' }, { text: 'г.Нижний Новгород' }],
-      ],
-      resize_keyboard: true,
-    },
-  });
-});
-
-/* bot.on('callback_query', (msg) => {
-  const chatId = msg.message.chat.id;
-  const id = msg.data;
-  getWeather(chatId, id);
-}); */
-
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-  let id;
-  if (msg.text !== undefined) {
-    if (msg.text === 'г.Химки') {
-      id = '550280';
-    } else if (msg.text === 'п.Быково') {
-      id = '570298';
-    } else if (msg.text === 'г.Москва') {
-      id = '524894';
-    } else if (msg.text === 'г.Нижний Новгород') {
-      id = '520555';
-    } else return;
-    getWeather(chatId, id);
-  } else return;
-
-  /* bot.sendMessage(chatId, 'Ваше мнение очень важно для нас, но это не точно :)'); */
-});
-
-function setDate(suntime) {
-  const date = new Date(suntime * 1000);
-
-  if (date.getMinutes() < 10 && date.getHours() < 10) {
-    return `0${date.getHours()}:0${date.getMinutes()}`;
-  } else {
-    return `${date.getHours()}:${date.getMinutes()}`;
-  }
 }
 
-// eslint-disable-next-line no-console
-console.log('Бот запущен');
+function setDate(suntime) { // форматирование времени в читаемый формат
+  const date = new Date(suntime * 1000);
+  if (date.getMinutes() < 10 && date.getHours() < 10) {
+    return `0${date.getHours()-3}:0${date.getMinutes()}`;
+  } else {
+    return `${date.getHours()-3}:${date.getMinutes()}`;
+  }
+}
